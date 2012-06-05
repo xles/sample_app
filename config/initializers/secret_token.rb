efef0868287556218291c7fb941a45b75dc68dd0
xles <xles@mirakulix.org>
2012-06-05 18:41:19 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1373985af7e164085109d4d1c65bbd8e1bb73648af0c7ec07c3c4f0f92bb5a34d217b30c1616f4110f6f742d795da9eca50de225bb0a5a6cba3b5e226a9e147f'
